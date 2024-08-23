7e910b361dcb1057493ed904a81959e6260aa1d8
yukiokd0403 <gangtianyushu14@gmail.com>
2024-08-23 15:30:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <?php
+  $data=['name' =>'onion','price' =>200,'weight'=>160 ];
+  print_r($data);
+  ?>
+  </body>
+  </html>
