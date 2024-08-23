8e2108369400eef163bd84fe77ec8b99872587db
yukiokd0403 <gangtianyushu14@gmail.com>
2024-08-23 12:46:55 +0900

    Add files via upload

---
+++ htdocs/kadai_005/score.php
@@ -0,0 +1,13 @@
+<!DOTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    $numbers=[80,60,55,40,100,25,80,95,30,60]
+    $average=arry_sum($numbers)/ count($numbers);
+    echo $average;
+    ?>
+</body>
