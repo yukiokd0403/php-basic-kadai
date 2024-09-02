fa37fbb85279ff8e028550ebd3f72393ead0e7d9
yukiokd0403 <gangtianyushu14@gmail.com>
2024-09-02 18:15:20 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -19,15 +19,21 @@
       public function getName():string{
         return $this->name;
       }
-      public function show_price(){
-        echo $this->price;
+      public function getPrice():int{
+        return $this->price;
       }
       public function detail():string{
         return"Food:".$this->getName().",Price:".$this->getPrice()."円";
       }
+      public function show_price(){
+        echo $this->price."円";
       }
+      }
+      
       $food=new Food('ジャガイモ',250);
+      
       print_r($food);
+      $food->show_price();
     ?>
     </p>
     <p>
@@ -45,8 +51,8 @@
       public function getName():string{
         return $this->name;
       }
-      public function show_height(){
-        echo $this->height;
+      public function getHeight(){
+        return $this->height;
       }
       public function getWeight():int{
         return $this->weight;
@@ -54,9 +60,14 @@
       public function detail():string{
         return"Animal:".$this->getName().",Height:".$this->getHeight()."m,Weight:".$this->getWeight()."kg";
       }
+      public function show_height(){
+        echo $this->height."m";
+      }
     }
     $animal=new Animal('犬',1,10);
+    
     print_r($animal);
+    $animal->show_height();
     ?>
     </p>
   </body>
